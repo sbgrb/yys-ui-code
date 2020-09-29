3d615376cf8b9e0e3ce7bc89ac279101ece6e9b7
sbgrb <974487599@qq.com>
2020-09-29 12:58:16 +0800

    基本完成主页内容

---
+++ src/components/DialogDemo.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        Dialog功能
+    </div>
+</template>
---
+++ src/router.ts
@@ -0,0 +1,28 @@
+import {createWebHashHistory,createRouter} from 'vue-router'
+import Home from './Views/Home.vue';
+import Doc from './Views/Doc.vue'
+import SwitchDemo from './components/SwitchDemo.vue'
+import ButtonDemo from './components/ButtonDemo.vue'
+import DialogDemo from './components/DialogDemo.vue'
+import TabsDemo from './components/TabsDemo.vue'
+import DocDemo from './components/DocDemo.vue'
+
+
+const history = createWebHashHistory()
+export const router = createRouter({
+    history:history,
+    routes:[
+        {path:'/',component:Home},
+        {path:'/doc',component: Doc,
+            children:[
+                {path:'',component:DocDemo},
+                {path:'switch',component:SwitchDemo},
+                {path:'button',component:ButtonDemo},
+                {path:'dialog',component:DialogDemo},
+                {path:'tabs',component:TabsDemo}
+            ]}
+    ]
+})
+router.afterEach(() =>{
+
+})
