eaee1c5e25bb14a2fe7cf8b00c8378d50c6cfced
sbgrb <974487599@qq.com>
2020-10-03 23:52:17 +0800

    完成dialog基本样式

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,21 @@
 <template>
-    <div>
-        Dialog功能
-    </div>
+    <div>Dialog示例</div>
+    <Button @click='toogle'>toggle</Button>
+    <Dialog v-model:visible="x"></Dialog>
 </template>
+
+<script lang="ts">
+    import Dialog from '../lib/Dialog.vue'
+    import Button from "../lib/Button.vue";
+    import {ref} from 'vue'
+    export default {
+        components:{Dialog,Button},
+        setup(){
+            const x = ref(false)
+            const toogle = () =>{
+                x.value = !x.value
+            }
+            return{x,toogle}
+        }
+    }
+</script>
