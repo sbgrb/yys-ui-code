a10283df6f27aef2e3e35eefb7b17b482eea580c
sbgrb <974487599@qq.com>
2020-10-13 18:33:13 +0800

    Switch 组件优化

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,46 +1,21 @@
 <template>
 <div>
     <h1>Switch 组件示例</h1>
-    <div class="demo">
-        <h2>常规用法</h2>
-        <div class="demo-component">
-            <Switch1Demo/>
-        </div>
-        <div class="demo-actions">
-            <Button @click="codeVisble1 = !codeVisble1">查看代码</Button>
-        </div>
-        <div class="demo-code" v-if="codeVisble1">
-            <pre>{{Switch1Demo.__sourceCode}}</pre>
-        </div>
-    </div>
-    <div class="demo">
-        <h2>支持 disable</h2>
-        <div class="demo-component">
-            <Switch2Demo/>
-        </div>
-        <div class="demo-actions">
-            <Button @click="codeVisble2 = !codeVisble2">查看代码</Button>
-        </div>
-        <div class="demo-code" v-if="codeVisble2">
-            <pre>{{Switch2Demo.__sourceCode}}</pre>
-        </div>
-    </div>
+    <Demo :component="Switch1Demo"/>
+    <Demo :component="Switch2Demo"/>
 </div>
 </template>
 
 <script lang="ts">
-    import Switch from '../lib/Switch.vue'
     import {ref} from 'vue'
-    import Button from "../lib/Button.vue";
     import Switch1Demo from './Switch1.demo.vue'
     import Switch2Demo from './Switch2.demo.vue'
+    import Demo from "./Demo.vue";
     export default {
-        components:{Button, Switch,Switch1Demo,Switch2Demo},
+        components:{Demo},
         setup(){
             const bool = ref(false)
-            const codeVisble1 = ref(false)
-            const codeVisble2 = ref(false)
-            return{bool,Switch2Demo,Switch1Demo,codeVisble1,codeVisble2}
+            return{bool,Switch2Demo,Switch1Demo}
         }
     }
 </script>
--- src/router.ts
+++ src/router.ts
@@ -5,17 +5,26 @@ import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
-import DocDemo from './components/DocDemo.vue'
+import Markdown from './components/Markdown.vue'
+import intro from './markdown/Intro.md'
+import started from './markdown/Started.md'
+import install from './markdown/Install.md'
+import {h} from 'vue'
+
 
 
 const history = createWebHashHistory()
+const md = filename => h(Markdown,{path:`../markdown/${filename}.md`,key: filename})
 export const router = createRouter({
     history:history,
     routes:[
         {path:'/',component:Home},
         {path:'/doc',component: Doc,
             children:[
-                {path:'',component:DocDemo},
+                {path:'',redirect:'/doc/intro'},
+                {path:'intro',component:md('intro')},
+                {path:'started',component:md('started')},
+                {path:'install',component:md('install')},
                 {path:'switch',component:SwitchDemo},
                 {path:'button',component:ButtonDemo},
                 {path:'dialog',component:DialogDemo},
